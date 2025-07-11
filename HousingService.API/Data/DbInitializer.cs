e8bee90de8a3e1ce4d22c3c16fe95bc3584264a4
Miron Levichev <mironlevichev@gmail.com>
2025-07-11 13:05:03 +0300

    Исправлено заполнение тестовыми данными

--- HousingService.API/Data/DbInitializer.cs
+++ HousingService.API/Data/DbInitializer.cs
@@ -16,34 +16,34 @@ namespace HousingService.API.Data
 
             var cities = new Cities[]
             {
-                new Cities { Id = 1, Name = "Ярославль" },
-                new Cities { Id = 2, Name = "Москва" },
-                new Cities { Id = 3, Name = "Новосибирск" },
-                new Cities { Id = 4, Name = "Архангельск" },
-                new Cities { Id = 5, Name = "Курск" }
+                new Cities { Name = "Ярославль" },
+                new Cities {Name = "Москва" },
+                new Cities {  Name = "Новосибирск" },
+                new Cities { Name = "Архангельск" },
+                new Cities { Name = "Курск" }
             };
             context.Cities.AddRange(cities);
             context.SaveChanges();
 
             var streets = new Streets[]
             {
-                new Streets { Id = 1, Name = "Пушкина", CityId = 1 },
-                new Streets { Id = 2, Name = "Пожарского", CityId = 1 },
-                new Streets { Id = 3, Name = "Ленина", CityId = 2 },
-                new Streets { Id = 4, Name = "Гоголя", CityId = 3 },
-                new Streets { Id = 5, Name = "Минина", CityId = 4 },
-                new Streets { Id = 6, Name = "Космонавтов", CityId = 5 }
+                new Streets { Name = "Пушкина", CityId = 1 },
+                new Streets {  Name = "Пожарского", CityId = 1 },
+                new Streets { Name = "Ленина", CityId = 2 },
+                new Streets { Name = "Гоголя", CityId = 3 },
+                new Streets { Name = "Минина", CityId = 4 },
+                new Streets { Name = "Космонавтов", CityId = 5 }
             };
             context.Streets.AddRange(streets);
             context.SaveChanges();
 
             var houses = new Houses[]
             {
-                new Houses { Id = 1, Number = "12", StreetId = 1 },
-                new Houses { Id = 2, Number = "2", StreetId = 2 },
-                new Houses { Id = 3, Number = "45А", StreetId = 3 },
-                new Houses { Id = 4, Number = "1А", StreetId = 4 },
-                new Houses { Id = 5, Number = "2В", StreetId = 5 }
+                new Houses { Number = "12", StreetId = 1 },
+                new Houses { Number = "2", StreetId = 2 },
+                new Houses { Number = "45А", StreetId = 3 },
+                new Houses { Number = "1А", StreetId = 4 },
+                new Houses { Number = "2В", StreetId = 5 }
             };
             context.Houses.AddRange(houses);
             context.SaveChanges();
